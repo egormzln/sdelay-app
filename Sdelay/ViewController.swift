dc2f12b678ce3d942d71078de226af295d9df3d2
Egor Mzln <miziulin_e@goldapple.ru>
2025-04-19 14:28:36 +0300

    Initial Commit

---
+++ Sdelay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sdelay
+//
+//  Created by Егор Мизюлин on 19.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
